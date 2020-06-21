2498ab24db09bc60d106ff8dd04095c70607fd09
André <amatos497@gmail.com>
2020-06-21 19:56:30 -0300

    commit ate aula 10.8. Adicionando tooltip

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
-import {TabViewModule} from 'primeng/tabview';
+import {InputTextModule} from 'primeng/inputtext';
 import {ButtonModule} from 'primeng/button';
+import {TableModule} from 'primeng/table';
+
 
 
 @NgModule({
   declarations: [
     AppComponent
-
   ],
   imports: [
     BrowserModule,
-    TabViewModule,
-    ButtonModule
+    InputTextModule,
+    ButtonModule,
+    TableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
